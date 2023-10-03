e2ac616ae307cc353eedd0c3d0045654c9c0d367
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-10-03 11:57:18 -0700

    added more files and boilerplate, installed rest of npm packages needed

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+// export const ADD_PROFILE = gql`
+//   mutation addProfile($name: String!, $email: String!, $password: String!) {
+//     addProfile(name: $name, email: $email, password: $password) {
+//       token
+//       profile {
+//         _id
+//         name
+//       }
+//     }
+//   }
+// `;
---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+//require models
+
+//module.exports = { model, model }
